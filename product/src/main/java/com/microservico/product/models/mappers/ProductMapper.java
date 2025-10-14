b0ff38073bcca3bf42acef01cb3bf2080ec687f1
Igor Souza <88636711+igorsouzaccruz@users.noreply.github.com>
2025-10-13 23:18:36 -0300

    feat(mapper): create manual mapper for product.

--- product/src/main/java/com/microservico/product/models/mappers/ProductMapper.java
+++ product/src/main/java/com/microservico/product/models/mappers/ProductMapper.java
@@ -1,2 +1,41 @@
-package com.microservico.product.models.mappers;public class ProductMapper {
+package com.microservico.product.models.mappers;
+
+import com.microservico.product.models.Product;
+import com.microservico.product.models.dtos.ProductDTO;
+import org.springframework.stereotype.Component;
+
+import java.util.Objects;
+
+@Component
+public class ProductMapper {
+
+    public ProductDTO toDTO(Product product) {
+
+        if (Objects.isNull(product))
+            return null;
+
+        return new ProductDTO(
+                product.getId(),
+                product.getDescription(),
+                product.getCategory(),
+                product.getPrice());
+    }
+
+    public Product toEntity(ProductDTO dto) {
+
+        if (Objects.isNull(dto))
+            return null;
+
+        Product product = new Product();
+
+        if (Objects.nonNull(dto.id())) {
+            product.setId(dto.id());
+        }
+
+        product.setDescription(dto.description());
+        product.setCategory(dto.category());
+        product.setPrice(dto.price());
+
+        return product;
+    }
 }
